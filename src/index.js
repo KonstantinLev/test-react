374e64b7c9b5d82558aea8898d1ccb7456aed25b
Konstantin <x-stels@bk.ru>
2017-05-17 06:25:25 +0300

    Эксперименты

--- src/index.js
+++ src/index.js
@@ -6,19 +6,23 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component
 {
+    btnOnClick(event) {
+        console.log('Кнопку нажали', event.target);
+    }
+
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: 'red'}} className="test">
                 <h1>Its Working!</h1>
                 <h3>Какой-то текст</h3>
+                <button onClick={this.btnOnClick}>{this.props.children}</button>
             </div>
-
         );
     }
 
 }
 
 ReactDOM.render(
-    <App />,
+    <App>TEST!</App>,
     document.getElementById('app')
 );
